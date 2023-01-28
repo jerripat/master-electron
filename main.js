f11a62d59d545c506ca477121996778ca2d40307
jerripat <jerripatterson@gmail.com>
2023-01-27 21:25:00 -0600

    updates

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+import { app, BrowserWindow } from 'electron'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secondaryWindow
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
-  mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+function createWindow() {
+  ;(mainWindow = new BrowserWindow({
+    width: 800,
+    height: 500,
     webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
-      contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+      backgroundColor: '#2B2E3B',
+    },
+  })),
+    (secondaryWindow = new BrowserWindow({
+      width: 600,
+      height: 300,
+      webPreferences: {
+        nodeIntegration: true,
+        parent: mainWindow,
+        modal: true,
+      },
+    }))
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
+  secondaryWindow.loadFile('secondary.html')
 
+  //mainWindow.once('ready-to-show', mainWindow.show)
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
+  mainWindow.on('closed', () => {
     mainWindow = null
+  })
+  secondaryWindow.on('closed', () => {
+    secondaryWindow = null
   })
 }
 
